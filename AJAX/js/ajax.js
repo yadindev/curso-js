5e881c5d86fea94c78fca5f30b1ee06dfb08cc88
yadidev <yadinds123@gmail.com>
2023-01-16 22:15:32 -0500

    AJAX - Fetch

--- AJAX/js/ajax.js
+++ AJAX/js/ajax.js
@@ -7,7 +7,7 @@
     if (xhr.readyState !== 4) return;
 
     if (xhr.status >= 200 && xhr.status <= 299) {
-      console.log("exito");
+      // console.log("exito");
       // console.log(xhr.responseText);
       // $xhr.innerHTML = xhr.responseText;
       let json = JSON.parse(xhr.responseText);
@@ -20,7 +20,7 @@
       });
       $xhr.appendChild($fragment);
     } else {
-      console.log("error", xhr.status);
+      // console.log("error", xhr.status);
       let message = xhr.statusText || "Ocurrio un error";
       $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
     }
@@ -30,4 +30,31 @@
   // xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
 
   xhr.send();
+})();
+
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+  fetch("./js/users.json")
+    // .then((res) => {
+    //   console.log(res);
+    //   return res.json();
+    // })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      console.log(json);
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      console.log(err);
+      let message = err.statusText || "Ocurrio un error";
+      $fetch.innerHTML = `Error ${err.status}: ${message}`;
+    })
+    .finally();
 })();
